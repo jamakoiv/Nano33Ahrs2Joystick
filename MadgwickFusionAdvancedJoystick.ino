88de1c881ec865842cbfd441ac0a343e6f47f3dc
Jaakko Koivisto <jaakko.m.koivisto@gmail.com>
2023-05-30 21:46:57 +0300

    Added even more output options and option to set magnetometer gain. Some code cleanup as well.

--- MadgwickFusionAdvancedJoystick.ino
+++ MadgwickFusionAdvancedJoystick.ino
@@ -12,26 +12,47 @@ USBJoystick joystick;
 
 enum {
     SERIAL_PRINT_NOTHING = 0x10,
-    SERIAL_PRINT_MAGNETIC = 0x11,
-    SERIAL_PRINT_NORMAL = 0x12,
-    SERIAL_SET_CALIBRATION = 0x30,
-    SERIAL_GET_CALIBRATION = 0x31,
+    SERIAL_PRINT_MAG_RAW = 0x11,
+    SERIAL_PRINT_MAG_CALIB = 0x12,
+    SERIAL_PRINT_ACC_RAW = 0x13,
+    SERIAL_PRINT_ACC_CALIB = 0x14,
+    SERIAL_PRINT_GYRO_RAW = 0x15,
+    SERIAL_PRINT_GYRO_CALIB = 0x16,
+    SERIAL_PRINT_AHRS = 0x20,
 
+    SERIAL_MAG_SET_OFFSET = 0x30,
+    SERIAL_MAG_GET_OFFSET = 0x31,
+    SERIAL_MAG_SET_GAIN = 0x32,
+    SERIAL_MAG_GET_GAIN = 0x33,
+    SERIAL_ACC_SET_OFFSET = 0x40,
+    SERIAL_ACC_GET_OFFSET = 0x41,
+    SERIAL_ACC_SET_GAIN = 0x42,
+    SERIAL_ACC_GET_GAIN = 0x43,
+    SERIAL_GYRO_SET_OFFSET = 0x50,
+    SERIAL_GYRO_GET_OFFSET = 0x51,
+    SERIAL_GYRO_SET_GAIN = 0x52,
+    SERIAL_GYRO_GET_GAIN = 0x53,
+
+    SERIAL_BAUDRATE = 57600,
     SERIAL_READ_BUFFER_SIZE = 65,
     SERIAL_COMMAND_SIZE = 4
 } ;
-auto SerialOutputMode = SERIAL_PRINT_NORMAL;
+auto SerialOutputMode = SERIAL_PRINT_AHRS;
 
 
-// Fusion-library objects and variables.
+/*
+  Fusion-library objects and variables.
+*/
 const uint32_t SAMPLE_RATE = 238;
 FusionOffset offset;
 FusionAhrs AHRS;
 
-
 FusionVector gyroscope;
 FusionVector accelerometer;
 FusionVector magnetometer; 
+
+uint32_t IMU_timeStamp, IMU_previousTimeStamp;
+float deltaTime;
 
 // Set AHRS algorithm settings
 const FusionAhrsSettings AHRSsettings = {
@@ -42,11 +63,9 @@ const FusionAhrsSettings AHRSsettings = {
         //.rejectionTimeout = 0
 } ;
 
-uint32_t IMU_timeStamp, IMU_previousTimeStamp;
-float deltaTime;
-
-
-// IMU measurement variables and calibration.
+/*
+ IMU measurement variables and calibration.
+*/
 using MyVector::vector;
 
 // Variables for acceleration values and calibrations.
@@ -54,7 +73,9 @@ vector Acc;
 vector rawAcc;
 vector CurrentAcc;
 vector AccGain(1.00f, 1.00f, 1.00f);
-vector AccOffset(0.0325f, 0.0306f, 0.01819f); const float ACC_INTEG = 0.90f;
+vector AccOffset(0.0325f, 0.0306f, 0.01819f); 
+const float ACC_INTEG = 0.90f;
+
 // Variables for gyroscope values and calibrations.
 vector Gyro;
 vector rawGyro;
@@ -72,15 +93,13 @@ vector rawMag;
 vector CurrentMag;
 //vector MagGain(1.0f/42.119f, 1.0f/40.031f, 1.0f/42.930f);
 //vector MagOffset(-11.660f, -10.046f, 1.094f);
-vector MagGain(1.0f/44.767f, 1.0f/44.074f, 1.0f/42.064f);
+//vector MagGain(1.0f/44.767f, 1.0f/44.074f, 1.0f/42.064f);
+vector MagGain(1.0f, 1.0f, 1.0f);
 vector MagOffset(-7.257f, 39.747f, -11.817f);
 const float MAG_INTEG = 0.90f;
 
-
-/*
-  Read and smooth the IMU-data. Uses leaky integrator smoothing.
-*/
 FusionVector readAcceleration(void) {
+    /* Read and smooth the IMU-data. Uses leaky integrator smoothing. */
     IMU.readAcceleration( Acc.x, Acc.y, Acc.z );
     Acc = (Acc + AccOffset) * AccGain;
     Acc = changeAxisSign(Acc, -1, -1, 1);
@@ -101,118 +120,109 @@ FusionVector readGyroscope(void) {
 FusionVector readMagneticField(vector* rawMag) {
     //IMU.readMagneticField( Mag.x, Mag.y, Mag.z );
     IMU.readMagneticField( rawMag->x, rawMag->y, rawMag->z );
-    Mag = (*rawMag + MagOffset) * MagGain;
+    // Serial.println(rawMag->x);
+    Mag = (*rawMag - MagOffset) * MagGain;
     Mag = changeAxisSign( Mag, -1, 1, -1 );
     CurrentMag = CurrentMag*MAG_INTEG + Mag*(1-MAG_INTEG);
+    // Serial.println(CurrentMag.x);
 
     return MyVector_to_FusionVector( CurrentMag );
 }
 
-/*
- Convert from MyVector::vector to FusionVector-object.
-*/
-FusionVector MyVector_to_FusionVector(const vector& vec) {
-  FusionVector res;
+inline FusionVector MyVector_to_FusionVector(const vector& vec) {
+    /* Convert from MyVector::vector to FusionVector-object. */
+    FusionVector res;
 
-  res.axis.x = vec.x;
-  res.axis.y = vec.y;
-  res.axis.z = vec.z;
+    res.axis.x = vec.x;
+    res.axis.y = vec.y;
+    res.axis.z = vec.z;
 
-  return res;
+    return res;
 }
 
-/*
-  Change the axis-signs.
-*/
 inline vector changeAxisSign(const vector& vec, int xSign, int ySign, int zSign) {
-  return vector( vec.x * xSign, vec.y * ySign, vec.z * zSign );
+    return vector( vec.x * xSign, vec.y * ySign, vec.z * zSign );
 }
+
+
 
 void printAHRSeuler(void) {
-  const FusionEuler euler = FusionQuaternionToEuler( FusionAhrsGetQuaternion( &AHRS ) );
+    const FusionEuler euler = FusionQuaternionToEuler( FusionAhrsGetQuaternion( &AHRS ) );
 
-  Serial.print( String("Roll: ") + String(euler.angle.roll, 2) + String(", ") );
-  Serial.print( String("Pitch: ") + String(euler.angle.pitch, 2) + String(", ") );
-  Serial.print( String("Yaw: ") + String(euler.angle.yaw, 2) + String(", ") );
+    Serial.print( String("Roll: ") + String(euler.angle.roll, 2) + String(", ") );
+    Serial.print( String("Pitch: ") + String(euler.angle.pitch, 2) + String(", ") );
+    Serial.print( String("Yaw: ") + String(euler.angle.yaw, 2) + String(", ") );
 }
 
 void printAHRSearth(void) {
-  const FusionVector earth = FusionAhrsGetEarthAcceleration( &AHRS );
+    const FusionVector earth = FusionAhrsGetEarthAcceleration( &AHRS );
 
-  Serial.print( String("X: ") + String(earth.axis.x, 2) + String(", ") ); 
-  Serial.print( String("Y: ") + String(earth.axis.y, 2) + String(", ") ); 
-  Serial.print( String("Z: ") + String(earth.axis.z, 2) + String(", ") ); 
+    Serial.print( String("X: ") + String(earth.axis.x, 2) + String(", ") ); 
+    Serial.print( String("Y: ") + String(earth.axis.y, 2) + String(", ") ); 
+    Serial.print( String("Z: ") + String(earth.axis.z, 2) + String(", ") ); 
 }
 
-// Calculate timestep and convert from microseconds to seconds.
 void updateTimeStamp(void) {
-  static const float MICROSECONDS_TO_SECONDS = 1.0f / 1000000.0f;
+    /* Calculate timestep and convert from microseconds to seconds. */
+    static const float MICROSECONDS_TO_SECONDS = 1.0f / 1000000.0f;
 
-  // Variables defined globally, bad...
-  IMU_timeStamp = micros();
-  deltaTime = static_cast<float>( (IMU_timeStamp - IMU_previousTimeStamp) * MICROSECONDS_TO_SECONDS ); 
-  IMU_previousTimeStamp = IMU_timeStamp;
+    // Variables defined globally, bad...
+    IMU_timeStamp = micros();
+    deltaTime = static_cast<float>( (IMU_timeStamp - IMU_previousTimeStamp) * MICROSECONDS_TO_SECONDS ); 
+    IMU_previousTimeStamp = IMU_timeStamp;
 }
 
-/*
- Update the joystick-axes using the AHRS angle data.
-*/
 void updateJoystickAxes(const FusionAhrs *const ahrs) {
-  FusionEuler euler = FusionQuaternionToEuler( FusionAhrsGetQuaternion( ahrs ));
+    /*Update the joystick-axes using the AHRS angle data. */
+    FusionEuler euler = FusionQuaternionToEuler( FusionAhrsGetQuaternion( ahrs ));
 
-  joystick.setXAxis( euler.angle.yaw );
-  joystick.setYAxis( euler.angle.pitch );
-  joystick.setZAxis( euler.angle.roll );
+    joystick.setXAxis( euler.angle.yaw );
+    joystick.setYAxis( euler.angle.pitch );
+    joystick.setZAxis( euler.angle.roll );
 
-  joystick.update();
+    joystick.update();
 }
-
 
 uint32_t printTimer;
+
 
 /*
   SETUP
 */
 void setup() {
+    Serial.begin(SERIAL_BAUDRATE);
 
-  Serial.begin(57600);
+    IMU.setGyroscopeSettings( LSM9DS1_ODR_G_238HZ, LSM9DS1_FS_G_500DPS );
+    IMU.setAccelerometerSettings( LSM9DS1_ODR_XL_119HZ, LSM9DS1_FS_XL_4G );
+    IMU.begin(); // Start the STM LSM9DS1 inertial unit.
 
-  IMU.setGyroscopeSettings( LSM9DS1_ODR_G_238HZ, LSM9DS1_FS_G_500DPS );
-  IMU.setAccelerometerSettings( LSM9DS1_ODR_XL_119HZ, LSM9DS1_FS_XL_4G );
-  IMU.begin(); // Start the STM LSM9DS1 inertial unit.
+    // Madgwick fusion library initialization.
+    FusionOffsetInitialise( &offset, SAMPLE_RATE );
+    FusionAhrsInitialise( &AHRS );
+    FusionAhrsSetSettings( &AHRS, &AHRSsettings );
 
-  // Madgwick fusion library initialization.
-  FusionOffsetInitialise( &offset, SAMPLE_RATE );
-  FusionAhrsInitialise( &AHRS );
-  FusionAhrsSetSettings( &AHRS, &AHRSsettings );
+    joystick.autoSend = false;
+    joystick.sendBlocking = false;
+    joystick.setXAxisRange( -180, 180 );  // left-right, yaw-axis. Range [-180, 180] degrees. 
+    joystick.setYAxisRange( -90, 90 );    // up-down, pitch-axis. Range [-90, 90] degrees.
+    joystick.setZAxisRange( -90, 90 );  // roll left-right, roll-axis. Range [-90, 90] degrees.
 
-  joystick.autoSend = false;
-  joystick.sendBlocking = false;
-  joystick.setXAxisRange( -180, 180 );  // left-right, yaw-axis. Range [-180, 180] degrees. 
-  joystick.setYAxisRange( -90, 90 );    // up-down, pitch-axis. Range [-90, 90] degrees.
-  joystick.setZAxisRange( -90, 90 );  // roll left-right, roll-axis. Range [-90, 90] degrees.
-
-  //joystick.setXAxisRange( -90, 90 );  // left-right, yaw-axis. Range [-90, 90] degrees. 
-  //joystick.setYAxisRange( -90, 90 );    // up-down, pitch-axis. Range [-90, 90] degrees.
-  //joystick.setZAxisRange( -180, 180 );  // roll left-right, roll-axis. Range [-180, 180] degrees.
+    //joystick.setXAxisRange( -90, 90 );  // left-right, yaw-axis. Range [-90, 90] degrees. 
+    //joystick.setYAxisRange( -90, 90 );    // up-down, pitch-axis. Range [-90, 90] degrees.
+    //joystick.setZAxisRange( -180, 180 );  // roll left-right, roll-axis. Range [-180, 180] degrees.
 
 
-  IMU_previousTimeStamp = micros();
-  printTimer = millis();
+    IMU_previousTimeStamp = micros();
+    printTimer = millis();
 
-  Serial.println("System reset.");
+    Serial.println("System reset.");
 }
-
-
-
 
 void loop() {
 
 #ifndef NO_MAGNETOMETER
-  /* 
-   If acceleration, gyroscopy and magnetometer data are ready. 
-   Accelerometer and gyroscope run with higher sample rate, magnetometer with 20 Hz.
-  */
+  /* If acceleration, gyroscopy and magnetometer data are ready. 
+   Accelerometer and gyroscope run with higher sample rate, magnetometer with 20 Hz. */
   if (IMU.accelerationAvailable() && IMU.gyroscopeAvailable() && IMU.magneticFieldAvailable()) {
     updateTimeStamp(); // Updates deltaTime variable.
 
@@ -229,16 +239,13 @@ void loop() {
     updateJoystickAxes( &AHRS );
   }
 
-  /* 
-    If acceleration and gyroscope data are ready.
-  */
+  /* If acceleration and gyroscope data are ready. */
   else if (IMU.accelerationAvailable() && IMU.gyroscopeAvailable()) {
 #endif
 #ifdef NO_MAGNETOMETER
   if (IMU.accelerationAvailable() && IMU.gyroscopeAvailable()) {
 #endif
     updateTimeStamp(); // Updates deltaTime variable.
-
     accelerometer = readAcceleration();   
     gyroscope = readGyroscope();
     
@@ -255,16 +262,27 @@ void loop() {
   if (millis() - printTimer > 100) {
     printTimer = millis();
 
-    if (SerialOutputMode == SERIAL_PRINT_NORMAL) {
-      printAHRSeuler();
-      Serial.print( String("Heading: ") );
-      Serial.println( FusionCompassCalculateHeading(accelerometer, magnetometer), 3 );
-    }
-    else if (SerialOutputMode == SERIAL_PRINT_MAGNETIC) {
-      rawMag.printVector();
+    switch (SerialOutputMode) {
+      case SERIAL_PRINT_AHRS:
+        printAHRSeuler();
+        Serial.print( String("Heading: ") );
+        Serial.println( FusionCompassCalculateHeading(accelerometer, magnetometer), 3 );
+        break;
+      case SERIAL_PRINT_MAG_RAW:
+        rawMag.printVector();
+        break;
+      case SERIAL_PRINT_MAG_CALIB:
+        Serial.print("x: ");
+        Serial.print(magnetometer.axis.x);
+        Serial.print(", y: ");
+        Serial.print(magnetometer.axis.y);
+        Serial.print(", z: ");
+        Serial.println(magnetometer.axis.z);
+        break;
     }
 
-    // Check for commands
+
+    /* Check for commands in serial. */
     if (Serial.available()) {
       delay(50);  // Small delay in case the received message is 
                   // incomplete when we check Serial.available. 
@@ -277,21 +295,51 @@ void loop() {
         case SERIAL_PRINT_NOTHING:
           SerialOutputMode = SERIAL_PRINT_NOTHING;
           break;
-        case SERIAL_PRINT_MAGNETIC:
-          SerialOutputMode = SERIAL_PRINT_MAGNETIC;
+
+        case SERIAL_PRINT_MAG_RAW:
+          SerialOutputMode = SERIAL_PRINT_MAG_RAW;
           break;
-        case SERIAL_PRINT_NORMAL:
-          SerialOutputMode = SERIAL_PRINT_NORMAL;
+
+        case SERIAL_PRINT_MAG_CALIB:
+          SerialOutputMode = SERIAL_PRINT_MAG_CALIB;
           break;
-        case SERIAL_SET_CALIBRATION:
+
+        case SERIAL_PRINT_AHRS:
+          SerialOutputMode = SERIAL_PRINT_AHRS;
+          break;
+
+        case SERIAL_MAG_SET_OFFSET:
           SerialOutputMode = SERIAL_PRINT_NOTHING;
           MagOffset.x = Serial.parseFloat();
           MagOffset.y = Serial.parseFloat();
           MagOffset.z = Serial.parseFloat();
           break;
-        case SERIAL_GET_CALIBRATION:
+
+        case SERIAL_MAG_SET_GAIN:
           SerialOutputMode = SERIAL_PRINT_NOTHING;
-          MagOffset.printVector();
+          MagGain.x = 1.0f/Serial.parseFloat();
+          MagGain.y = 1.0f/Serial.parseFloat();
+          MagGain.z = 1.0f/Serial.parseFloat();
+          break;
+
+        case SERIAL_MAG_GET_OFFSET:
+          SerialOutputMode = SERIAL_PRINT_NOTHING;
+          Serial.print("x: ");
+          Serial.print(MagOffset.x);
+          Serial.print(", y: ");
+          Serial.print(MagOffset.y);
+          Serial.print(", z: ");
+          Serial.println(MagOffset.z);
+          break;
+
+        case SERIAL_MAG_GET_GAIN:
+          SerialOutputMode = SERIAL_PRINT_NOTHING;
+          Serial.print(", x: ");
+          Serial.print(1.0f/MagGain.x);
+          Serial.print(", y: ");
+          Serial.print(1.0f/MagGain.y);
+          Serial.print(", z: ");
+          Serial.println(1.0f/MagGain.z);
           break;
       }
 
