e75abeba174aeef2723ef7669fd9387132ad72f0
Jaakko Koivisto <jaakko.m.koivisto@gmail.com>
2025-04-23 16:11:33 +0300

    LSP changed intendations.

--- src/MyVector/MyVector.cpp
+++ src/MyVector/MyVector.cpp
@@ -6,18 +6,17 @@
 using MyVector::vector;
 
 // CONSTRUCTORS AND DESTRUCTORS
-vector::vector() { }
+vector::vector() {}
 
-vector::vector(const float x, const float y, const float z) 
-  : x(x), y(y), z(z) { }
+vector::vector(const float x, const float y, const float z)
+    : x(x), y(y), z(z) {}
 
-vector::~vector() { }
-
+vector::~vector() {}
 
 // METHODS //
 
 double vector::norm(void) const {
-  return sqrt( pow(x,2) + pow(y,2) + pow(z,2) );
+  return sqrt(pow(x, 2) + pow(y, 2) + pow(z, 2));
 }
 
 void vector::normalize(void) {
@@ -29,75 +28,74 @@ void vector::normalize(void) {
 }
 
 std::string vector::to_string(void) const {
-  std::string str = std::to_string(this->x) + std::string(", ") + 
-                    std::to_string(this->y) + std::string(", ") + 
+  std::string str = std::to_string(this->x) + std::string(", ") +
+                    std::to_string(this->y) + std::string(", ") +
                     std::to_string(this->z);
   return str;
 }
 
-vector vector::crossProduct(const vector& A, const vector &B) {
-  double X = A.y*B.z - A.z*B.y;
-  double Y = A.z*B.x - A.x*B.z;
-  double Z = A.x*B.y - A.y*B.x;
+vector vector::crossProduct(const vector &A, const vector &B) {
+  double X = A.y * B.z - A.z * B.y;
+  double Y = A.z * B.x - A.x * B.z;
+  double Z = A.x * B.y - A.y * B.x;
 
   return vector(X, Y, Z);
 }
 
 double vector::dotProduct(const vector &A, const vector &B) {
-  return A.x*B.x + A.y*B.y + A.z+B.z;
-} 
+  return A.x * B.x + A.y * B.y + A.z + B.z;
+}
 
 double vector::vectorAngle(const vector &A, const vector &B) {
-  return acos( vector::dotProduct(A,B) / (A.norm()*B.norm()) );
+  return acos(vector::dotProduct(A, B) / (A.norm() * B.norm()));
 }
 
 // OPERATORS //
 
-vector vector::operator + (const vector& A) {  
-  return vector( this->x + A.x, this->y + A.y, this->z + A.z );
+vector vector::operator+(const vector &A) {
+  return vector(this->x + A.x, this->y + A.y, this->z + A.z);
 }
-vector& vector::operator += (const vector &A) {
-  this->x += A.x; 
-  this->y += A.y; 
-  this->z += A.z; 
+vector &vector::operator+=(const vector &A) {
+  this->x += A.x;
+  this->y += A.y;
+  this->z += A.z;
   return *this;
 }
 
-vector vector::operator - (const vector &A) {
-  return vector( this->x - A.x, this->y - A.y, this->z - A.z );
+vector vector::operator-(const vector &A) {
+  return vector(this->x - A.x, this->y - A.y, this->z - A.z);
 }
-vector& vector::operator -= (const vector &A) {
+vector &vector::operator-=(const vector &A) {
   this->x -= A.x;
   this->y -= A.y;
   this->z -= A.z;
   return *this;
 }
 
-vector vector::operator * (const vector &A) {
-  return vector( this->x * A.x, this->y * A.y, this->z * A.z );
+vector vector::operator*(const vector &A) {
+  return vector(this->x * A.x, this->y * A.y, this->z * A.z);
 }
 
-vector vector::operator * (const float C) {
-  return vector( this->x * C, this->y * C, this->z * C );
+vector vector::operator*(const float C) {
+  return vector(this->x * C, this->y * C, this->z * C);
 }
-vector& vector::operator *= (const float C) {
+vector &vector::operator*=(const float C) {
   this->x *= C;
   this->y *= C;
   this->z *= C;
   return *this;
 }
 
-vector vector::operator / (const vector& A) {
-  return vector( this->x / A.x, this->y / A.y, this->z / A.z );
+vector vector::operator/(const vector &A) {
+  return vector(this->x / A.x, this->y / A.y, this->z / A.z);
 }
 
-vector vector::operator / (const float C) {
-  return vector( this->x / C, this->y / C, this->z / C );
+vector vector::operator/(const float C) {
+  return vector(this->x / C, this->y / C, this->z / C);
 }
-vector& vector::operator /= (const float C) {
+vector &vector::operator/=(const float C) {
   this->x /= C;
   this->y /= C;
   this->z /= C;
   return *this;
 }
-
