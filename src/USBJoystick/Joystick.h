5ecad1c9afcce793857c9d10ed4f89eafefefc92
Jaakko Koivisto <jaakko.m.koivisto@gmail.com>
2025-04-28 16:48:54 +0300

    Changed axis_t to use union datatype. Makes it easier to loop over the data if necessary.

--- src/USBJoystick/Joystick.h
+++ src/USBJoystick/Joystick.h
@@ -3,16 +3,21 @@
 
 namespace arduino {
 
-struct axis_t {
-  float X;
-  float Y;
-  float Z;
-  float Rx;
-  float Ry;
-  float Rz;
-  float slider0;
-  float slider1;
-};
+typedef union {
+  float array[8];
+
+  struct axis_t {
+    float X;
+    float Y;
+    float Z;
+    float Rx;
+    float Ry;
+    float Rz;
+    float slider0;
+    float slider1;
+
+  } name;
+} axis_t;
 
 enum { X, Y, Z, Rx, Ry, Rz, slider0, slider1 };
 
@@ -23,16 +28,26 @@ private:
   static const uint8_t BYTE_LENGTH = 8;
   static const uint8_t BUTTONS_MAX_NUMBER =
       64; // Actual max amount of buttons we are using.
-  //
-  uint8_t buttonState[BUTTONS_MAX_NUMBER];
+  static const uint8_t AXIS_AMOUNT = 8;
+
+  // TODO: Replace MBED_ASSERTS with c++ assert.
+  // Buttons amount must be byte-aligned because I don't want to deal with
+  // padding-data in the HID-report :)
+  // MBED_STATIC_ASSERT(BUTTONS_MAX_NUMBER % BYTE_LENGTH == 0,
+  //                    "BUTTONS_MAX_NUMBER does not align to byte-length");
 
   axis_t axis;
   axis_t axisMin;
   axis_t axisMax;
 
 public:
+  uint8_t buttonState[BUTTONS_MAX_NUMBER];
+
   Joystick();
   ~Joystick();
+
+  uint8_t getButtonBytesAmount(void);
+  uint8_t getAxisAmount(void);
 
   void setAxis(float value, int AXIS);
   void setAxisRange(float minimum, float maximum, int AXIS);
