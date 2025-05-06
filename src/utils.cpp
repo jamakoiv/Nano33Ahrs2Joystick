5542109948284b6339f26bf6337b65133b102ffd
Jaakko Koivisto <jaakko.m.koivisto@gmail.com>
2025-05-06 14:13:20 +0300

    Removed old string-handling functions.

--- src/utils.cpp
+++ src/utils.cpp
@@ -1,7 +1,5 @@
-#include <Serial.h>
-#include <sstream>
-
 #include "utils.h"
+#include <sstream>
 
 std::vector<std::string> split(const std::string &s, char delim) {
     std::vector<std::string> res;
@@ -33,47 +31,6 @@ std::vector<std::string> split_between(const std::string &s, char start_char,
 
         std::string substr = s.substr(pos0, pos1 - pos0 + 1);
         res.push_back(substr);
-    }
-
-    return res;
-}
-
-std::vector<std::string> split_input(std::string input,
-                                     const std::string &delimiter) {
-    /*
-      Split string at 'delimiter' and return the pieces in a
-      std::vector<std::string>.
-    */
-    std::vector<std::string> res;
-
-    auto pos = input.find(delimiter);
-
-    while (pos != std::string::npos) {
-        res.emplace_back(input.substr(0, pos));
-        input.erase(0, pos + 1);
-        pos = input.find(delimiter);
-    }
-
-    // Handle the last element if there is no delimiter at the end of the input
-    // string.
-    if (input.empty()) {
-        return res;
-    } else {
-        res.emplace_back(input);
-        return res;
-    }
-}
-
-std::vector<float> split_and_strtof(std::string input,
-                                    const std::string &delimiter) {
-    /*
-      Split string, convert elements to float and return them as
-      std::vector<float>.
-    */
-    std::vector<float> res;
-    auto input_str_vec = split_input(input, delimiter);
-    for (std::string str : input_str_vec) {
-        res.emplace_back(strtof(str.c_str(), NULL));
     }
 
     return res;
--- src/utils.h
+++ src/utils.h
@@ -6,10 +6,9 @@
 
 #include "MyVector/MyVector.h"
 
-std::vector<std::string> split_input(std::string input,
-                                     const std::string &delimiter);
-std::vector<float> split_and_strtof(std::string input,
-                                    const std::string &delimiter);
+std::vector<std::string> split(const std::string &s, char delim);
+std::vector<std::string> split_between(const std::string &s, char start_char,
+                                       char stop_char);
 
 void set_calib_helper(const std::vector<float> &data, MyVector::vector &offset);
 void set_calib_helper(const std::vector<float> &data, MyVector::vector &offset,
