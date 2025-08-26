2991064ad57979c48609ab30a755879ad9c1e760
Jaakko Koivisto <jaakko.m.koivisto@gmail.com>
2025-08-26 16:52:13 +0300

    Added tests for the create_message and retrieve_command.

--- test/test_serial_utils.cpp
+++ test/test_serial_utils.cpp
@@ -30,21 +30,6 @@ void test_parse_inbound_bytes() {
     assert(correct == res);
 }
 
-void test_create_message() {
-    string header{0x31, 0x05};
-    string body{0x20, 0x21, 0x22, 0x23, 0x24};
-
-    string correct{0x01, 0x31, 0x05, 0x02, 0x20, 0x21,
-                   0x22, 0x23, 0x24, 0x03, 0x04};
-
-    string res = create_message(header, body);
-
-    print_hex(correct);
-    print_hex(res);
-
-    assert(res == correct);
-}
-
 void test_retrieve_header_and_body() {
     string msg{0x01, 0x31, 0x05, 0x02, 0x20, 0x21,
                0x22, 0x23, 0x24, 0x03, 0x04};
@@ -86,17 +71,17 @@ void test_sanity_check_message_fail2() {
     assert(res == -2);
 }
 
-void test_command_2_strings() {
+void test_command2bytes() {
     command_t cmd;
     cmd.id = 0x31;
-    cmd.n_bytes = 14;
+    cmd.n_params = 3;
     cmd.params = {1.11, 2.22, 3.33};
 
-    std::string correct_header{0x31, 0x0E};
+    std::string correct_header{0x31, 0x03};
     std::string correct_body(
         "\x7b\x14\x8e\x3f\x7b\x14\x0e\x40\xb8\x1e\x55\x40");
 
-    auto [header, body] = command_2_strings(cmd);
+    auto [header, body] = command2bytes(cmd);
 
     assert(header == correct_header);
     assert(body == correct_body);
@@ -107,19 +92,19 @@ void test_command_2_strings() {
     print_hex(body);
 }
 
-void test_strings_2_command() {
-    std::string header{0x31, 0x0E};
+void test_bytes2command() {
+    std::string header{0x31, 0x03};
     std::string body("\x7b\x14\x8e\x3f\x7b\x14\x0e\x40\xb8\x1e\x55\x40");
 
     command_t correct_cmd;
     correct_cmd.id = 0x31;
-    correct_cmd.n_bytes = 14;
+    correct_cmd.n_bytes = 3;
     correct_cmd.params = {1.11, 2.22, 3.33};
 
-    command_t res = strings_2_command(header, body);
+    command_t res = bytes2command(header, body);
 
     assert(res.id == correct_cmd.id);
-    assert(res.n_bytes == correct_cmd.n_bytes);
+    assert(res.n_params == correct_cmd.n_params);
 
     for (float f : res.params) {
         std::cout << f << std::endl;
@@ -131,11 +116,71 @@ void test_strings_2_command() {
     assert(res.params[2] == correct_cmd.params[2]);
 }
 
+void test_create_message() {
+    // INFO: Test without ESCAPE-bytes.
+    command_t cmd;
+    cmd.id = 0x31;
+    cmd.n_params = 5;
+    cmd.params = {1.11, 1.11, 1.11, 1.11, 1.11};
+
+    string correct("\x01\x31\x05\x02\x7b\x14\x8e\x3f\x7b\x14\x8e\x3f\x7b\x14"
+                   "\x8e\x3f\x7b\x14\x8e\x3f\x7b\x14\x8e\x3f\x03\x04");
+    string res = create_message(cmd);
+
+    std::cout << "[test_create_message] correct: ";
+    print_hex(correct);
+    std::cout << "[test_create_message] create_message: ";
+    print_hex(res);
+
+    assert(correct == res);
+}
+
+void test_create_message_escape() {
+    // INFO: Test with ESCAPE-bytes.
+    command_t cmd;
+    cmd.id = 0x02;
+    cmd.n_params = 3;
+    cmd.params = {0.5117493, 1.11, 1.11};
+
+    string correct("\x01\x1b\x22\x1b\x23\x02\x1b\x21\x1b\x22\x1b\x23\x3f\x7b"
+                   "\x14\x8e\x3f\x7b\x14\x8e\x3f\x03\x04");
+    string res = create_message(cmd);
+
+    std::cout << "[test_create_message_escape] correct: ";
+    print_hex(correct);
+    std::cout << "[test_create_message_escape] create_message: ";
+    print_hex(res);
+
+    assert(correct == res);
+}
+
+void test_retrieve_command() {
+    string msg("\x01\x31\x05\x02\x7b\x14\x8e\x3f\x7b\x14\x8e\x3f\x7b\x14"
+               "\x8e\x3f\x7b\x14\x8e\x3f\x7b\x14\x8e\x3f\x03\x04");
+
+    command_t correct_cmd = {0x31, 5, 5, {1.11, 1.11, 1.11, 1.11, 1.11}};
+    command_t res = retrieve_command(msg);
+
+    assert(res.id == correct_cmd.id);
+    assert(res.n_params == correct_cmd.n_params);
+    assert(res.params == std::vector<float>({1.11, 1.11, 1.11, 1.11, 1.11}));
+}
+
+void test_retrieve_command_escape() {
+    string msg("\x01\x1b\x22\x1b\x23\x02\x1b\x21\x1b\x22\x1b\x23\x3f\x7b"
+               "\x14\x8e\x3f\x7b\x14\x8e\x3f\x03\x04");
+
+    command_t correct_cmd = {0x02, 3, 3, {0.5117493, 1.11, 1.11}};
+    command_t res = retrieve_command(msg);
+
+    assert(res.id == correct_cmd.id);
+    assert(res.n_params == correct_cmd.n_params);
+    assert(res.params == std::vector<float>({0.5117493, 1.11, 1.11}));
+}
+
 int main() {
     test_parse_inbound_bytes();
     test_parse_outbound_bytes();
-
-    test_create_message();
 
     test_retrieve_header_and_body();
 
@@ -143,6 +188,12 @@ int main() {
     test_sanity_check_message_fail1();
     test_sanity_check_message_fail2();
 
-    test_command_2_strings();
-    test_strings_2_command();
+    test_command2bytes();
+    test_bytes2command();
+
+    test_create_message();
+    test_create_message_escape();
+
+    test_retrieve_command();
+    test_retrieve_command_escape();
 }
